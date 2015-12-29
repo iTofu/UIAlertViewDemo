c75b41d19fafa0a30d80dd8a52175551c7e487a6
Leo <leoios@sina.com>
2015-12-29 18:06:24 +0800

    Add test code

--- UIAlertViewDemo/ViewController.m
+++ UIAlertViewDemo/ViewController.m
@@ -8,20 +8,54 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define KEY_WINDOW  [UIApplication sharedApplication].keyWindow
+
+@interface ViewController () <UIAlertViewDelegate>
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Title"
+                                                        message:@"message"
+                                                       delegate:self
+                                              cancelButtonTitle:@"ok"
+                                              otherButtonTitles:nil, nil];
+    
+    NSLog(@"origin key window: %p", KEY_WINDOW);
+    
+    [alertView show];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UIAlertView Delegate
+
+/**
+ *  点 alertView 上面的 btn 时调用，alertView 还在
+ */
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    NSLog(@"when clicked key window: %p", KEY_WINDOW);
+}
+
+/**
+ *  alertView 将要消失时调用
+ */
+- (void)alertView:(UIAlertView *)alertView willDismissWithButtonIndex:(NSInteger)buttonIndex {
+    
+    NSLog(@"alert view will dismiss key window: %p", KEY_WINDOW);
+}
+/**
+ *  alertView 已经消失时调用
+ */
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    
+    NSLog(@"alert view did dismiss key window: %p", KEY_WINDOW);
 }
 
 @end
